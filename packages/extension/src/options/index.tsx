db9e9fc610259207ef0b1fa6583c435972138b26
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-11-11 18:14:39 +0530

    Use correct jsx tags

--- packages/extension/src/options/index.tsx
+++ packages/extension/src/options/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './options';
+import Options from './options';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Options />
 	</StrictMode>
 );
