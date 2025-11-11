d936c3022c557cfc5c3639706be7eacacefed5bc
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-11-11 17:34:45 +0530

    Add initial extension files including icons, SVG, and manifest
    - Added icon images in various sizes (16x16, 32x32, 48x48) for the extension.
    - Included an SVG representation of the extension's icon.
    - Created a manifest.json file defining the extension's metadata, permissions, and structure.
    - Set up the extension for incognito mode and specified background service worker.
    - Configured web accessible resources for data and report files.

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 import path, { resolve } from 'path';
@@ -13,36 +14,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const packagesDir = resolve(__dirname, '../');
-const aliases = readdirSync(packagesDir).map((name) => {
-  if(name === 'shared-config'){
-	return {
-		find: `@google-awlt/${name}`,
-  		replacement: resolve(packagesDir, name),
-	}
-  }else {
-	return {
-		find: `@google-awlt/${name}`,
-  		replacement: resolve(packagesDir, name, 'src'),
-	}
-  }
-});
+const aliases = readdirSync(packagesDir).filter(name => name !== 'shared-config').map((name) => ({
+	find: `@google-awlt/${name}`,
+	replacement: resolve(packagesDir, name, 'src'),
+}));
 const distDir = path.resolve(__dirname, '../../dist/extension');
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const isDev = process.env.NODE_ENV === 'development';
+console.log(resolve(__dirname, 'src/options/options.html'))
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	plugins: [react(), tailwindcss(), svgr()],
+	plugins: [react(), tailwindcss(), svgr(), 
+        viteStaticCopy({
+          targets: [
+            { src: 'manifest.json', dest: '' },
+            { src: 'icons', dest: '' },
+          ],
+        }),
+      ],
 	resolve: {
 		alias: aliases,
 	},
 	base: '',
+	root: 'src',
 	build: {
 		emptyOutDir: false,
 		watch: isDev ? {} : null,
 		sourcemap: isDev ? true : false,
-        minify: !isDev,
+		minify: !isDev,
+		outDir: distDir,
 		rollupOptions: {
 			input: {
 				options: resolve(__dirname, 'src/options/options.html'),
@@ -51,8 +53,9 @@ export default defineConfig({
 				popup: resolve(__dirname, 'src/popup/popup.html'),
 			},
 			output: {
-				dir: path.join(distDir, '[name]'),
-              	entryFileNames: '[name].js',
+				entryFileNames: '[name]/[name].js',
+				chunkFileNames: '[name]/[name]-chunk.js',
+                assetFileNames: '[name]/[name]-assets.[ext]',
 			}
 		}
 	}
