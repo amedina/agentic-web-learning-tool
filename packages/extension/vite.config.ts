fcb4d1d6f3d9bb639803b057a8c038ed69655263
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-11-11 16:02:19 +0530

    refactor: update import paths and improve structure for extension components
    - Changed import paths for assets and components in devtools, options, popup, and sidePanel.
    - Updated index files to use the correct component names.
    - Enhanced HTML structure in options, popup, and devtools with proper DOCTYPE and meta tags.
    - Modified Vite configuration to support multiple entry points and added SVGR plugin for SVG handling.
    - Added Jest and Playwright configuration files for testing setup.
    - Introduced devtools setup script to manage panel visibility in Chrome DevTools.
    - Added setup files for Jest testing with appropriate imports.

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -2,20 +2,58 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
-import path from 'path';
+import path, { resolve } from 'path';
+import { readdirSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packagesDir = resolve(__dirname, '../');
+const aliases = readdirSync(packagesDir).map((name) => {
+  if(name === 'shared-config'){
+	return {
+		find: `@google-awlt/${name}`,
+  		replacement: resolve(packagesDir, name),
+	}
+  }else {
+	return {
+		find: `@google-awlt/${name}`,
+  		replacement: resolve(packagesDir, name, 'src'),
+	}
+  }
+});
+const distDir = path.resolve(__dirname, '../../dist/extension');
+
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+const isDev = process.env.NODE_ENV === 'development';
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	server: {
-		port: 5174, // Force dev server to always use 5174.
-		strictPort: true, // Fail if 5173 is already taken.
-	},
+	plugins: [react(), tailwindcss(), svgr()],
 	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
+		alias: aliases,
 	},
+	base: '',
+	build: {
+		emptyOutDir: false,
+		watch: isDev ? {} : null,
+		sourcemap: isDev ? true : false,
+        minify: !isDev,
+		rollupOptions: {
+			input: {
+				options: resolve(__dirname, 'src/options/options.html'),
+				devtools: resolve(__dirname, 'src/devtools/devtools.html'),
+				sidePanel: resolve(__dirname, 'src/sidepanel/sidePanel.html'),
+				popup: resolve(__dirname, 'src/popup/popup.html'),
+			},
+			output: {
+				dir: path.join(distDir, '[name]'),
+              	entryFileNames: '[name].js',
+			}
+		}
+	}
 });
