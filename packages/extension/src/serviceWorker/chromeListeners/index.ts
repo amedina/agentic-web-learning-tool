8239940a08cf3798efd87ad6e741e29c50de7c3e
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-12-31 16:29:02 +0530

    Add tabOnActivated and tabOnCreated listener to configure sidepanel for better context management.

--- packages/extension/src/serviceWorker/chromeListeners/index.ts
+++ packages/extension/src/serviceWorker/chromeListeners/index.ts
@@ -4,7 +4,11 @@
 import syncStorageChangeCallback from "./syncStorageChangeCallback";
 import onInstalledCallback from "./onInstalledListener";
 import onActionClickedCallback from "./onActionClickCallback";
+import tabOnActivatedCallback from "./tabOnActivatedCallback";
+import tabOnCreatedCallback from "./tabOnCreatedCallback";
 
 chrome.storage.sync.onChanged.addListener(syncStorageChangeCallback);
 chrome.action.onClicked.addListener(onActionClickedCallback);
 chrome.runtime.onInstalled.addListener(onInstalledCallback);
+chrome.tabs.onActivated.addListener(tabOnActivatedCallback);
+chrome.tabs.onCreated.addListener(tabOnCreatedCallback);
