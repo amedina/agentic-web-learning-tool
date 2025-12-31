bba15b1958c037beb3738b8c4e15e9109ab12752
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2025-12-31 12:08:48 +0530

    refactor: improve formatting and structure of onInstalledCallback function

--- packages/extension/src/serviceWorker/chromeListeners/onInstalledCallback.ts
+++ packages/extension/src/serviceWorker/chromeListeners/onInstalledCallback.ts
@@ -1,24 +1,53 @@
 /**
  * Internal dependencies
  */
-import { settingsSetter, setLogLevelFromSyncSettings } from "../../utils";
-import type { AgentType, SettingsState } from "../../types";
-import { DEFAULT_SETTINGS } from "../../constants";
+import { settingsSetter, setLogLevelFromSyncSettings } from '../../utils';
+import type { AgentType, SettingsState } from '../../types';
+import { DEFAULT_SETTINGS } from '../../constants';
 
-const onInstalledCallback = async (details: chrome.runtime.InstalledDetails) => {
-    if (details.reason === 'install') {
-        chrome.storage.sync.set({ selectedAgent: { model: 'prompt-api', modelProvider: 'browser-ai' } })
-        Object.entries(DEFAULT_SETTINGS).map(([key, value]) => settingsSetter(key as keyof SettingsState, value))
-    }
+const onInstalledCallback = async (
+	details: chrome.runtime.InstalledDetails
+) => {
+	if (details.reason === 'install') {
+		chrome.storage.sync.set({
+			selectedAgent: { model: 'prompt-api', modelProvider: 'browser-ai' },
+		});
+		await Promise.all(
+			Object.entries(DEFAULT_SETTINGS).map(
+				async ([key, value]) =>
+					await settingsSetter(key as keyof SettingsState, value)
+			)
+		);
+	}
 
-    if (details.reason === 'update') {
-        const { selectedAgent }: { selectedAgent: AgentType } =
-            await chrome.storage.sync.get('selectedAgent');
-        if(!selectedAgent){
-            chrome.storage.sync.set({ selectedAgent: { model: 'prompt-api', modelProvider: 'browser-ai' } })
-        }
-    }
-    await setLogLevelFromSyncSettings();
+	if (details.reason === 'update') {
+		const { selectedAgent }: { selectedAgent: AgentType } =
+			await chrome.storage.sync.get('selectedAgent');
+		if (!selectedAgent) {
+			chrome.storage.sync.set({
+				selectedAgent: {
+					model: 'prompt-api',
+					modelProvider: 'browser-ai',
+				},
+			});
+		}
+		await chrome.storage.sync
+			.get('extensionSettings')
+			.then(async (data) => {
+				const json = JSON.parse(
+					(data.extensionSettings ?? '{}') as string
+				);
+				if (!json.theme && !json.logLevel) {
+					await chrome.storage.sync.set({
+						extensionSettings: JSON.stringify({
+							theme: 'auto',
+							logLevel: 'SILENT',
+						}),
+					});
+				}
+			});
+	}
+	await setLogLevelFromSyncSettings();
 };
 
 export default onInstalledCallback;
