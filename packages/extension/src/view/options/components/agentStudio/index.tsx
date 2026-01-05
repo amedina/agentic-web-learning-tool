17e510f5b31bdadc047c08aa9d5983070f44b30a
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2026-01-05 14:32:00 +0530

    Add key to accordion.

--- packages/extension/src/view/options/components/agentStudio/index.tsx
+++ packages/extension/src/view/options/components/agentStudio/index.tsx
@@ -25,6 +25,7 @@ export default function AgentDashboard() {
 					(provider) => provider.id !== 'browser-ai'
 				).map((provider) => (
 					<SingleProviderAccordion
+						key={provider.id}
 						provider={provider}
 						storedData={apiKeys?.[provider.id]}
 						apiKeys={apiKeys}
