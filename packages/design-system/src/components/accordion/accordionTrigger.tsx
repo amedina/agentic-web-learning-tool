67943ddbea43053e509a5f94e977ca020a23eef8
sayedtaqui <sayedwp@gmail.com>
2026-01-05 18:59:10 +0400

    Style accordion

--- packages/design-system/src/components/accordion/accordionTrigger.tsx
+++ packages/design-system/src/components/accordion/accordionTrigger.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import type { ComponentProps } from "react";
-import { Header, Trigger} from "@radix-ui/react-accordion";
+import { Header, Trigger } from "@radix-ui/react-accordion";
 import { ChevronDownIcon } from "lucide-react";
 /**
  * Internal dependencies
@@ -20,7 +20,7 @@ function AccordionTrigger({
       <Trigger
         data-slot="accordion-trigger"
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none [&[data-state=open]>svg]:rotate-180",
           className
         )}
         {...props}
