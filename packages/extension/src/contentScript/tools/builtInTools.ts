757c8bdb8985763d41ba739c586e9ce213770602
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2026-01-08 14:10:31 +0530

    Add extensionTool State in the installed callback

--- packages/extension/src/contentScript/tools/builtInTools.ts
+++ packages/extension/src/contentScript/tools/builtInTools.ts
@@ -1,65 +1,65 @@
 import {
-	BookmarksApiTools,
-	DomExtractionTools,
-	HistoryApiTools,
-	ScriptingApiTools,
-	StorageApiTools,
-	TabGroupsApiTools,
-	TabsApiTools,
-	WindowsApiTools,
+  BookmarksApiTools,
+  DomExtractionTools,
+  HistoryApiTools,
+  ScriptingApiTools,
+  StorageApiTools,
+  TabGroupsApiTools,
+  TabsApiTools,
+  WindowsApiTools,
 } from '@mcp-b/extension-tools';
 
 export const builtInTools = {
-	bookmarks: {
-		name: 'Bookmarks',
-		instance: BookmarksApiTools,
-		enabled: true,
-	},
-	domExtraction: {
-		name: 'Dom Extraction',
-		instance: DomExtractionTools,
-		enabled: true,
-	},
-	history: {
-		name: 'History',
-		instance: HistoryApiTools,
-		enabled: true,
-	},
-	scripting: {
-		name: 'Scripting',
-		instance: ScriptingApiTools,
-		options: {
-			executeScript: true,
-			executeUserScript: true,
-			insertCSS: false,
-			removeCSS: false,
-		},
-		enabled: true,
-	},
-	storage: {
-		name: 'Storage',
-		instance: StorageApiTools,
-		enabled: true,
-	},
-	tabGroups: {
-		name: 'Tab Groups',
-		instance: TabGroupsApiTools,
-		enabled: true,
-	},
-	tabs: {
-		name: 'Tabs',
-		instance: TabsApiTools,
-		options: {
-			getAllTabs: true,
-			createTab: true,
-			closeTabs: true,
-			updateTab: true,
-		},
-		enabled: true,
-	},
-	windows: {
-		name: 'Windows',
-		instance: WindowsApiTools,
-		enabled: true,
-	},
+  bookmarks: {
+    name: 'Bookmarks',
+    instance: BookmarksApiTools,
+    enabled: true,
+  },
+  domExtraction: {
+    name: 'Dom Extraction',
+    instance: DomExtractionTools,
+    enabled: true,
+  },
+  history: {
+    name: 'History',
+    instance: HistoryApiTools,
+    enabled: true,
+  },
+  scripting: {
+    name: 'Scripting',
+    instance: ScriptingApiTools,
+    options: {
+      executeScript: true,
+      executeUserScript: true,
+      insertCSS: false,
+      removeCSS: false,
+    },
+    enabled: true,
+  },
+  storage: {
+    name: 'Storage',
+    instance: StorageApiTools,
+    enabled: true,
+  },
+  tabGroups: {
+    name: 'Tab Groups',
+    instance: TabGroupsApiTools,
+    enabled: true,
+  },
+  tabs: {
+    name: 'Tabs',
+    instance: TabsApiTools,
+    options: {
+      getAllTabs: true,
+      createTab: true,
+      closeTabs: true,
+      updateTab: true,
+    },
+    enabled: true,
+  },
+  windows: {
+    name: 'Windows',
+    instance: WindowsApiTools,
+    enabled: true,
+  },
 };
--- packages/extension/src/serviceWorker/chromeListeners/onInstalledCallback.ts
+++ packages/extension/src/serviceWorker/chromeListeners/onInstalledCallback.ts
@@ -7,71 +7,70 @@ import { DEFAULT_SETTINGS } from '../../constants';
 import { builtInTools } from '@/contentScript/tools/builtInTools';
 
 const onInstalledCallback = async (
-	details: chrome.runtime.InstalledDetails
+  details: chrome.runtime.InstalledDetails
 ) => {
-	if (details.reason === 'install') {
-		chrome.storage.sync.set({
-			selectedAgent: { model: 'prompt-api', modelProvider: 'browser-ai' },
-		});
-		await Promise.all(
-			Object.entries(DEFAULT_SETTINGS).map(
-				async ([key, value]) =>
-					await settingsSetter(key as keyof SettingsState, value)
-			)
-		);
-		const extensionToolsState: { [key: string]: { enabled: boolean } } = {};
+  if (details.reason === 'install') {
+    chrome.storage.sync.set({
+      selectedAgent: { model: 'prompt-api', modelProvider: 'browser-ai' },
+    });
+    await Promise.all(
+      Object.entries(DEFAULT_SETTINGS).map(
+        async ([key, value]) =>
+          await settingsSetter(key as keyof SettingsState, value)
+      )
+    );
+    const extensionToolsState: { [key: string]: { enabled: boolean } } = {};
 
-		Object.keys(builtInTools).forEach(
-			(tool) => (extensionToolsState[tool].enabled = true)
-		);
+    Object.keys(builtInTools).forEach(
+      (tool) => (extensionToolsState[tool].enabled = true)
+    );
 
-		await chrome.storage.local.set({
-			extensionToolsState,
-		});
-	}
+    await chrome.storage.local.set({
+      extensionToolsState,
+    });
+  }
 
-	if (details.reason === 'update') {
-		const { selectedAgent }: { selectedAgent: AgentType } =
-			await chrome.storage.sync.get('selectedAgent');
-		if (!selectedAgent) {
-			chrome.storage.sync.set({
-				selectedAgent: {
-					model: 'prompt-api',
-					modelProvider: 'browser-ai',
-				},
-			});
-		}
-		chrome.storage.sync.get('extensionSettings').then(async (data) => {
-			const json = JSON.parse((data.extensionSettings ?? '{}') as string);
-			if (!json.theme && !json.logLevel) {
-				await chrome.storage.sync.set({
-					extensionSettings: JSON.stringify({
-						theme: 'auto',
-						logLevel: 'SILENT',
-					}),
-				});
-			}
-		});
+  if (details.reason === 'update') {
+    const { selectedAgent }: { selectedAgent: AgentType } =
+      await chrome.storage.sync.get('selectedAgent');
+    if (!selectedAgent) {
+      chrome.storage.sync.set({
+        selectedAgent: {
+          model: 'prompt-api',
+          modelProvider: 'browser-ai',
+        },
+      });
+    }
+    chrome.storage.sync.get('extensionSettings').then(async (data) => {
+      const json = JSON.parse((data.extensionSettings ?? '{}') as string);
+      if (!json.theme && !json.logLevel) {
+        await chrome.storage.sync.set({
+          extensionSettings: JSON.stringify({
+            theme: 'auto',
+            logLevel: 'SILENT',
+          }),
+        });
+      }
+    });
 
-		const { extensionToolsState } = await chrome.storage.local.get(
-			'extensionToolsState'
-		);
+    const { extensionToolsState } = await chrome.storage.local.get(
+      'extensionToolsState'
+    );
 
-		if (!extensionToolsState) {
-			const extensionToolsState: { [key: string]: { enabled: boolean } } =
-				{};
+    if (!extensionToolsState) {
+      const extensionToolsState: { [key: string]: { enabled: boolean } } = {};
 
-			Object.keys(builtInTools).forEach(
-				(tool) => (extensionToolsState[tool].enabled = true)
-			);
+      Object.keys(builtInTools).forEach(
+        (tool) => (extensionToolsState[tool].enabled = true)
+      );
 
-			await chrome.storage.local.set({
-				extensionToolsState,
-			});
-		}
-	}
+      await chrome.storage.local.set({
+        extensionToolsState,
+      });
+    }
+  }
 
-	await setLogLevelFromSyncSettings();
+  await setLogLevelFromSyncSettings();
 };
 
 export default onInstalledCallback;
