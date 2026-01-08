d2e0cda25cf0fa8247e13bc98e302414d8f31e78
sayedtaqui <sayedwp@gmail.com>
2026-01-08 17:19:18 +0400

    Improve isDomainAllowed function to handle main domain and add test coverage

--- packages/extension/tests/jest.setup.cjs
+++ packages/extension/tests/jest.setup.cjs
@@ -3,11 +3,11 @@
  */
 const React = require('react');
 const { TextEncoder, TextDecoder } = require('node:util');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 global.React = React;
 global.TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
-global.fetch = fetch;
-global.Request = fetch.Request;
-global.Response = fetch.Response;
+// global.fetch = fetch;
+// global.Request = fetch.Request;
+// global.Response = fetch.Response;
