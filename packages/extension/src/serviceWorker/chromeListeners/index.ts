a4c1c32cc2b8c95cc7f45c43fb310053a7318431
sayedtaqui <sayedwp@gmail.com>
2026-01-13 19:38:13 +0400

    Remove redundant code and add side_panel in manifest.json

--- packages/extension/src/serviceWorker/chromeListeners/index.ts
+++ packages/extension/src/serviceWorker/chromeListeners/index.ts
@@ -3,10 +3,6 @@
  */
 import syncStorageChangeCallback from './syncStorageChangeCallback';
 import onInstalledCallback from './onInstalledCallback';
-import tabOnActivatedCallback from './tabOnActivatedCallback';
-import tabOnCreatedCallback from './tabOnCreatedCallback';
 
 chrome.storage.sync.onChanged.addListener(syncStorageChangeCallback);
 chrome.runtime.onInstalled.addListener(onInstalledCallback);
-chrome.tabs.onActivated.addListener(tabOnActivatedCallback);
-chrome.tabs.onCreated.addListener(tabOnCreatedCallback);
