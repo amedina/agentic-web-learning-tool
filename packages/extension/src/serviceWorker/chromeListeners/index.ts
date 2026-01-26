6a6cbbf9e136dee0a40edad0cf02089996ede225
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2026-01-26 17:18:20 +0530

    Delete window thread on closing window.

--- packages/extension/src/serviceWorker/chromeListeners/index.ts
+++ packages/extension/src/serviceWorker/chromeListeners/index.ts
@@ -3,6 +3,8 @@
  */
 import syncStorageChangeCallback from './syncStorageChangeCallback';
 import onInstalledCallback from './onInstalledCallback';
+import onWindowClosedCallback from './onWindowClosedCallback';
 
 chrome.storage.sync.onChanged.addListener(syncStorageChangeCallback);
 chrome.runtime.onInstalled.addListener(onInstalledCallback);
+chrome.windows.onRemoved.addListener(onWindowClosedCallback);
