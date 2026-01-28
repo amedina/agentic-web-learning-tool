9542eeee50d11319caaaf3d451a2e4843854b021
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2026-01-28 22:05:19 +0530

    Remove deletion of therads when window is closed

--- packages/extension/src/serviceWorker/chromeListeners/index.ts
+++ packages/extension/src/serviceWorker/chromeListeners/index.ts
@@ -3,7 +3,6 @@
  */
 import syncStorageChangeCallback from './syncStorageChangeCallback';
 import onInstalledCallback from './onInstalledCallback';
-import onTabClosedCallback from './onTabClosedCallback';
 import tabOnActivatedCallback from './tabOnActivatedCallback';
 import tabOnCreatedCallback from './tabOnCreatedCallback';
 import onActionClickedCallback from './onActionClickedCallback';
@@ -11,7 +10,6 @@ import { START_MCP_CONNECTION } from '../../constants';
 
 chrome.storage.sync.onChanged.addListener(syncStorageChangeCallback);
 chrome.runtime.onInstalled.addListener(onInstalledCallback);
-chrome.tabs.onRemoved.addListener(onTabClosedCallback);
 chrome.tabs.onActivated.addListener(tabOnActivatedCallback);
 chrome.tabs.onCreated.addListener(tabOnCreatedCallback);
 chrome.action.onClicked.addListener(onActionClickedCallback);
