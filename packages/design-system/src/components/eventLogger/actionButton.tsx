2191949721c8296b1df01766c791f4653bf29db9
sayedtaqui <sayedwp@gmail.com>
2026-02-01 11:48:23 +0400

    Use lucid icon for play button

--- packages/design-system/src/components/eventLogger/actionButton.tsx
+++ packages/design-system/src/components/eventLogger/actionButton.tsx
@@ -1,3 +1,7 @@
+/**
+ * External dependencies.
+ */
+import { Play } from 'lucide-react';
 interface ActionButtonProps<T> {
   onRunTool: (tool: T) => void;
   details: T;
@@ -16,19 +20,7 @@ export const ActionButton = <T,>({
       }}
       title="Run Tool"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <polygon points="5 3 19 12 5 21 5 3" />
-      </svg>
+      <Play width="16" height="16" />
     </button>
   );
 };
