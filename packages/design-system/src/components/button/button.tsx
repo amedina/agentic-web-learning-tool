92c725c77f2c4740b36654bfa30a59d322b3d167
sayedtaqui <sayedwp@gmail.com>
2026-02-01 10:38:09 +0400

    Add new taditional blue color

--- packages/design-system/src/components/button/button.tsx
+++ packages/design-system/src/components/button/button.tsx
@@ -1,7 +1,13 @@
+/**
+ * External dependencies.
+ */
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
+/**
+ * Internal dependencies.
+ */
 import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
@@ -20,7 +26,7 @@ const buttonVariants = cva(
         ghost:
           'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
-        run: 'bg-[#3871e0] text-white shadow-xs hover:bg-[#3871e0]/90',
+        run: 'bg-traditional-blue text-white shadow-xs hover:bg-traditional-blue/90',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
