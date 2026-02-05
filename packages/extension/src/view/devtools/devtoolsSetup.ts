9304b55f7d01f5585ab4fcfea7ec2230f913745f
sayedtaqui <sayedwp@gmail.com>
2026-02-05 15:23:53 +0400

    Rename WebMCP Tools to WebMCP and remove legacy code

--- packages/extension/src/view/devtools/devtoolsSetup.ts
+++ packages/extension/src/view/devtools/devtoolsSetup.ts
@@ -1,33 +1,5 @@
-const callback = (panel: {
-  onShown: { addListener: (arg0: () => void) => void };
-  onHidden: { addListener: (arg0: () => void) => void };
-}) => {
-  // Fires when the user switches to the panel.
-  panel.onShown.addListener(async () => {
-    if (!chrome.runtime?.id) {
-      return;
-    }
-
-    await chrome.storage.session.set({
-      [chrome.devtools.inspectedWindow.tabId + '-privacySandboxPanelVisible']:
-        true,
-    });
-  });
-
-  panel.onHidden.addListener(async () => {
-    if (!chrome.runtime?.id) {
-      return;
-    }
-
-    await chrome.storage.session.set({
-      [chrome.devtools.inspectedWindow.tabId + '-privacySandboxPanelVisible']:
-        false,
-    });
-  });
-};
 chrome.devtools.panels.create(
-  'WebMCP Tools',
+  'WebMCP',
   'icons/icon.svg',
-  'devtools/index.html#' + chrome.devtools.inspectedWindow.tabId,
-  callback
+  'devtools/index.html#' + chrome.devtools.inspectedWindow.tabId
 );
