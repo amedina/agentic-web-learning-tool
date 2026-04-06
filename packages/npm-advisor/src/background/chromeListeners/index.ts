3ff169d8d463fa02f1e9d52cf855c7be94367d8d
Amoghavarsha Kudaligi <kgamoghavarsha@gmail.com>
2026-04-06 21:34:20 +0530

    feat: implement side panel auto-configuration on extension install and enable events

--- packages/npm-advisor/src/background/chromeListeners/index.ts
+++ packages/npm-advisor/src/background/chromeListeners/index.ts
@@ -3,6 +3,7 @@
  */
 import tabOnCreatedCallback from "./tabOnCreatedCallback";
 import onActionClickedCallback from "./onActionClickedCallback";
+import onInstalledCallback from "./onInstalledCallback";
 
 const openedTabs = new Set<number>();
 
@@ -10,3 +11,5 @@ chrome.tabs.onCreated.addListener(tabOnCreatedCallback);
 chrome.action.onClicked.addListener((event) =>
   onActionClickedCallback(event, openedTabs),
 );
+chrome.runtime.onInstalled.addListener(onInstalledCallback);
+chrome.management.onEnabled.addListener(onInstalledCallback);
