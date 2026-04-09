7376ad6c290ca01225749a4488749432f6bb6e5d
sayedtaqui <sayedwp@gmail.com>
2026-04-09 12:53:26 +0530

    Fix search list background issue

--- packages/npm-advisor/src/view/options/tabs/compare/search/resultsList.tsx
+++ packages/npm-advisor/src/view/options/tabs/compare/search/resultsList.tsx
@@ -18,7 +18,7 @@ export const ResultsList = ({
   const { hits } = useHits();
 
   return (
-    <div className="absolute left-0 right-0 mt-2 bg-bg-surface dark:bg-aswad border border-subtle-zinc dark:border-darth-vader rounded-xl shadow-2xl z-50 max-h-72 overflow-y-auto glass border-opacity-50 transition-all animate-in fade-in slide-in-from-top-2 duration-300">
+    <div className="absolute left-0 right-0 mt-2 bg-white dark:bg-aswad border border-subtle-zinc dark:border-darth-vader rounded-xl shadow-2xl z-50 max-h-72 overflow-y-auto glass border-opacity-50 transition-all animate-in fade-in slide-in-from-top-2 duration-300">
       {hits.map((hit) => (
         <Hit
           key={hit.objectID}
